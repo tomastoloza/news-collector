63a5e11f74523b98fb9f426b0b437ac9d8fde0fa
tomastoloza <tomasezequieltoloza@gmail.com>
2019-09-23 22:32:35 -0300

    Add xml skeleton

--- main/news_collector.py
+++ main/news_collector.py
@@ -1,13 +1,9 @@
 import configparser
-import time
-import xml
+import os
 from urllib import request
 from urllib.error import URLError
-from xml.etree.ElementTree import ElementTree, tostring, fromstring, Element
-from xml.dom import minidom
-from urllib.request import urlopen
-
-import feedparser
+from xml.etree.ElementTree import ElementTree, tostring, fromstring, Element, SubElement
+from xml.etree import ElementTree
 
 
 class NewsCollector(object):
@@ -18,44 +14,53 @@ class NewsCollector(object):
     # TODO: return tuple of (url, name) pairs
     def get_rss_urls(self):
         rss_list = []
-        items = []
+        default_size = len(self.parser.defaults())
         for section in self.parser.sections():
-            items.append(self.parser.items(section))
-        for item in items:
-            for rss in [item[x][1] for x in range(4, len(item))]:
-                rss_list.append(item[3][1] + rss)
+            item = self.parser.items(section)
+            for num in range(default_size + 1, len(item)):
+                rss_list.append((section, item[num][0], item[default_size][1] + item[num][1]))
         return rss_list
-
-    def get_entries(self):
-        entries = []
-        for rss in (self.get_rss_urls()):
-            feed = feedparser.parse(rss)
-            for entry in feed["entries"]:
-                entries.append(entry)
-        return entries
 
     def create_element_tree(self):
         try:
+            print(self.get_rss_urls())
             for url in self.get_rss_urls():
-                resource = request.urlopen(url)
+                print(url)
+                resource = request.urlopen(url[2])
                 element = fromstring(resource.read())
-                with open("../resources/rss/" + str(element) + ".xml", 'w+') as file:
+                print(url[0])
+                if not os.path.exists("../resources/rss/" + url[0]):
+                    os.mkdir("../resources/rss/" + url[0])
+                with open("../resources/rss/" + url[0] + "/" + url[1] + ".xml", 'w+') as file:
                     root = Element("news")
                     tree = ElementTree(root)
-                    print(tree.getroot())
                     for a in element:
                         for b in a:
-                            new = Element("news_item")
+                            new = Element("news:item")
                             for c in b:
                                 if c.tag == "title" or c.tag == "description" or c.tag == "pubDate":
                                     new.append(c)
                                 root.append(new)
-                    # tree.write(file, encoding="unicode")
+                    tree.write(file, encoding="unicode")
         except URLError as e:
             print(e)
+
+    def fuckin_tree(self):
+        for rss in self.get_rss_urls():
+            resource = request.urlopen(rss[2])
+            element = fromstring(resource.read())
+            # if not os.path.exists("../resources/rss/" + rss[0]):
+            #     os.makedirs("../resources/rss/" + rss[0])
+            # with open("../resources/rss/" + rss[0] + "/" + rss[1] + ".xml", 'w+') as file:
+            feed = element.findall(".//item/")
+            news = Element("news")
+            news_item = SubElement(news, "news:item")
+            title = feed.(".//title")
+            print(title)
 
 
 if __name__ == '__main__':
     nc = NewsCollector()
-    nc.create_element_tree()
-    # nc.create_xml()
+    # nc.create_element_tree()
+    # nc.get_rss_urls()
+    nc.fuckin_tree()
