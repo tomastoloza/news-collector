2a8d62c65bd0e22c8946eab5bd4c9385431e0c50
tomastoloza <tomasezequieltoloza@gmail.com>
2019-09-25 21:46:23 -0300

    Update xml parser

--- main/news_collector.py
+++ main/news_collector.py
@@ -1,9 +1,9 @@
 import configparser
 import os
 from urllib import request
-from urllib.error import URLError
-from xml.etree.ElementTree import ElementTree, tostring, fromstring, Element, SubElement
 from xml.etree import ElementTree
+from xml.etree.ElementTree import ElementTree, fromstring, Element
+import xml.etree.ElementTree as ET
 
 
 class NewsCollector(object):
@@ -28,24 +28,33 @@ class NewsCollector(object):
             req = request.Request(url=rss[2], headers=header)
             result = request.urlopen(req)
             element = fromstring(result.read())
-            if not os.path.exists("../resources/rss/" + rss[0]):
-                os.makedirs("../resources/rss/" + rss[0])
-            with open("../resources/rss/" + rss[0] + "/" + rss[1] + ".xml", 'w+') as file:
-                print("../resources/rss/" + rss[0] + "/" + rss[1] + ".xml")
-                root = Element("news")
-                tree = ElementTree.ElementTree(root)
+            path = "../resources/rss/" + rss[0]
+
+            print(rss[2])
+            root = Element("news")
+            tree = ElementTree(root)
+            if not os.path.exists(path):
+                os.makedirs(path)
+            with open(path + "/" + rss[1] + ".xml", 'w+') as file:
+                read = file.read()
                 for item in element.findall(".//item"):
                     new = Element("item")
                     for tag in item:
                         if tag.tag == "title" or tag.tag == "description" or tag.tag == "pubDate":
                             new.append(tag)
                     root.append(new)
-                tree.write(file, encoding="unicode")
+                try:
+                    xml_parsed = fromstring((path + "/" + rss[1] + ".xml"))
+                    print(xml_parsed)
+                except ET.ParseError as e:
+                    print(e)
+                finally:
+                    tree.write(file, encoding="unicode", xml_declaration=True)
 
-    # def tuher(self):
-    #     with open("../resources/rss/TN/la viola.xml", "r") as file:
-    #         xml_parsed = ElementTree.ElementTree().parse(file, parser=None)
-    #     print(xml_parsed)
+    def tuher(self):
+        with open("../resources/rss/TN/la viola.xml", "r") as file:
+            xml_parsed = ElementTree().parse(file, parser=None)
+            print(xml_parsed[0][0].text)
 
 
 if __name__ == '__main__':
