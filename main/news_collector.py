af5b1e9c2d1a9d1b5f77c976a772de53037f27e8
tomastoloza <tomasezequieltoloza@gmail.com>
2019-10-15 00:28:26 -0300

    Fix collector

--- main/news_collector.py
+++ main/news_collector.py
@@ -11,7 +11,6 @@ class NewsCollector(object):
         self.parser = configparser.ConfigParser()
         self.parser.read("../config.ini")
 
-    # TODO: return tuple of (url, name) pairs
     def get_rss_urls(self):
         rss_list = []
         default_size = len(self.parser.defaults())
@@ -21,48 +20,43 @@ class NewsCollector(object):
                 rss_list.append((section, item[num][0], item[default_size][1] + item[num][1]))
         return rss_list
 
-    def save_rss_from_url(self):
+    def get_and_compare(self):
         for rss in self.get_rss_urls():
+            print(rss)
+            file_name = "../resources/rss/" + rss[0] + "/" + rss[1] + ".xml"
             header = {
                 'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"}
             req = request.Request(url=rss[2], headers=header)
             result = request.urlopen(req)
-            path = "../resources/temp/rss/" + rss[0]
             element = fromstring(result.read())
-            root = Element("news")
-            tree = ElementTree(root)
-            print(rss[2])
+            try:
+                xml_parsed = ET.parse(file_name)
+            except FileNotFoundError:
+                root = Element("news")
+                xml_parsed = ElementTree(root)
+                os.makedirs("../resources/rss/" + rss[0])
+                open(file_name, 'w+')
+
             for item in element.findall(".//item"):
-                new = Element("item")
+                news_item = Element("item")
                 for tag in item:
                     if tag.tag == "title" or tag.tag == "description" or tag.tag == "pubDate":
-                        new.append(tag)
-                root.append(new)
-            if not os.path.exists(path):
-                os.makedirs(path)
-            with open(path + "/" + rss[1] + ".xml", 'w+') as file:
-                tree.write(file, encoding="unicode", xml_declaration=False)
+                        news_item.append(tag)
+                if not self.find_item(news_item, file_name):
+                    xml_parsed.getroot().append(news_item)
+            xml_parsed.write(file_name, encoding='UTF-8')
 
-    def read_and_compare(self):
-        for rss in self.get_rss_urls():
-            path = "../resources/rss/" + rss[0]
-            path_temp = "../resources/temp/rss/" + rss[0]
-            with open(path_temp + "/" + rss[1] + ".xml", 'r') as file:
-                if file.read() != '':
-                    try:
-                        xml_parsed = ET.parse(path_temp + "/" + rss[1] + ".xml")
-                        for item_element in xml_parsed.getroot():
-                            for x in item_element:
-                                if x.tag == 'title':
-                                    print(x.text)
-                    except ET.ParseError as e:
-                        e.with_traceback()
-                else:
-                    xml_parsed
-                if not os.path.exists(path):
-                    os.makedirs(path)
-                with open(path + "/" + rss[1] + ".xml", 'w+') as file_read:
-                    xml_parsed.write(file_read, encoding='unicode')
+    def find_item(self, item, file_name):
+        try:
+            if open(file_name, 'r').read() != '':
+                xml_parsed = ET.parse(file_name)
+                for x in xml_parsed.getroot():
+                    # TODO: verificacion por fecha
+                    if x[0].text == item[0].text:
+                        return True
+            return False
+        except ET.ParseError as e:
+            e.with_traceback()
 
 
 if __name__ == '__main__':
@@ -71,4 +65,5 @@ if __name__ == '__main__':
     # nc.get_rss_urls()
     # nc.fuckin_tree()
     # nc.save_rss_from_url()
-    nc.read_and_compare()
+    # nc.read_and_compare()
+    nc.get_and_compare()
